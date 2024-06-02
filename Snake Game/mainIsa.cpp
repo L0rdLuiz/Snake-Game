24d6149d86d863c09ee60890bc40ddb5d404ee68
Isabela Silvério <145703412+Isabela-Silverio-Cardoso-Pereira@users.noreply.github.com>
2024-06-02 16:37:43 -0300

    Update mainIsa.cpp
    implementação do menu

--- Snake Game/mainIsa.cpp
+++ Snake Game/mainIsa.cpp
@@ -4,6 +4,18 @@
 
 using namespace std;
 
+bool contemApenasLetras(const string& str)   // verifica se o nome contém apenas letras
+{
+    for (char c : str)
+    {
+        if (!isalpha(c))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main()
 {
     ///ALERTA: NAO MODIFICAR O TRECHO DE CODIGO, A SEGUIR.
@@ -23,97 +35,159 @@ int main()
     ///ALERTA: NAO MODIFICAR O TRECHO DE CODIGO, ACIMA.
 
     int menu;
+    string nome;
     int repetir = 0;
 
-    do {
-        //Colocar nome do jogo
-        //Colocar as opções no cout: jogar 1, sobre o jogo 2, rank 3, e sair no 4
+    do
+    {
+        cout << "   ____ , __     ___  |   ,   ___         ___.   ___  , _ , _     ___" << endl;
+        cout << "  (     |'  `.  /   ` |  /  .'   `      .'   `  /   ` |' `|' `. .'   `" << endl;
+        cout << "  `--.  |    | |    | |-<   |----'      |    | |    | |   |   | |----'" << endl;
+        cout << " \\___.' /    | `.__/| /  \\_ `.___,       `---| `.__/| /   '   / `.___," <<endl;
+        cout << "                                         \\___/" << endl << endl << endl << endl<< endl;
+
+        cout <<"                          ______________________________ " << endl;
+        cout << "                         |                              |" << endl;
+        cout << "                         |         1 - COMECAR          |" << endl;
+        cout << "                         |         2 - TUTORIAL         |" << endl;
+        cout << "                         |         3 - RANKING          |" << endl;
+        cout << "                         |         4 - SAIR             |" << endl;
+        cout << "                         |______________________________|" << endl;
+
 
         cin >> menu;
-        switch (menu) {
-            case 1: //Jogo
-                system("cls");
-                do {
-                    bool jogo = true;
+        switch (menu)
+        {
+        case 1: //Jogo
+            cout << "digite seu nome: " << endl; //nome do jogador para o rank
+            cin >> nome;
+            if (!contemApenasLetras(nome))
+            {
+                cout << "Erro: O nome deve conter apenas letras." << endl;
+                cout << "digite seu nome:" << endl;
+                cin >> nome;
+            }
 
-                    int m[15][17] = {
-                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-                    };
+            system("cls");
+            do
+            {
+                bool jogo = true;
 
-                    //Posicao inicial do personagem no console
-                    int x = 5, y = 5;
-                    //Variavel para tecla precionada
-                    char tecla;
+                int m[15][17] =
+                {
+                    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+                };
 
-                    while (jogo == true) {
-                        ///Posiciona a escrita no inicio do console
-                        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+                //Posicao inicial do personagem no console
+                int x = 5, y = 5;
+                //Variavel para tecla precionada
+                char tecla;
 
-                        ///Imprime o jogo: mapa e personagem.
-                        for (int i = 0; i < 15; i++) {
-                            for (int j = 0; j < 17; j++) {
-                                if (i == x && j == y) {
-                                    cout << char(36); //personagem
-                                } else {
-                                    switch (m[i][j]) {
-                                        case 0: cout << " "; break; //caminho
-                                        case 1: cout << char(219); break; //parede
-                                        //default: cout << "-"; //erro
-                                    } //fim switch
-                                }
+                while (jogo == true)
+                {
+                    ///Posiciona a escrita no inicio do console
+                    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+
+                    ///Imprime o jogo: mapa e personagem.
+                    for (int i = 0; i < 15; i++)
+                    {
+                        for (int j = 0; j < 17; j++)
+                        {
+                            if (i == x && j == y)
+                            {
+                                cout << char(36); //personagem
                             }
-                            cout << "\n";
-                        } //fim for mapa
-
-                        ///executa os movimentos
-                        if (_kbhit()) {
-                            tecla = getch();
-                            switch (tecla) {
-                                case 72: case 'w': ///cima
-                                    x--;
-                                    break;
-                                case 80: case 's': ///baixo
-                                    x++;
-                                    break;
-                                case 75: case 'a': ///esquerda
-                                    y--;
-                                    break;
-                                case 77: case 'd': ///direita
-                                    y++;
-                                    break;
+                            else
+                            {
+                                switch (m[i][j])
+                                {
+                                case 0:
+                                    cout << " ";
+                                    break; //caminho
+                                case 1:
+                                    cout << char(219);
+                                    break; //parede
+                                    //default: cout << "-"; //erro
+                                } //fim switch
                             }
                         }
-                    }; //fim do laco do jogo
-                } while (repetir == 1);
-                break;
-            case 2: //Sobre o jogo
+                        cout << "\n";
+                    } //fim for mapa
 
-                break;
-            case 3: //Rank
+                    ///executa os movimentos
+                    if (_kbhit())
+                    {
+                        tecla = getch();
+                        switch (tecla)
+                        {
+                        case 72:
+                        case 'w': ///cima
+                            x--;
+                            break;
+                        case 80:
+                        case 's': ///baixo
+                            x++;
+                            break;
+                        case 75:
+                        case 'a': ///esquerda
+                            y--;
+                            break;
+                        case 77:
+                        case 'd': ///direita
+                            y++;
+                            break;
+                        }
+                    }
+                }; //fim do laco do jogo
+            }
+            while (repetir == 1);
+            break;
+        case 2: //Sobre o jogo
+            system ("cls");
 
-                break;
-            case 4: // Obrigado por jogar
 
-                break;
-            default:
-                cout << "Esta opcao nao e aceita digite outro numero";
-                break;
+            cout<< "                                                                  " << endl;
+
+            cout<< "                                                                  " << endl;
+
+            cout << "OBJETIVO: Coma 100 macas sem colidir com a parede ou com a cobra." << endl;
+            cout << "MOVIMENTO: Use W, A, S e D para mover a cobra que se move sempre para frente" << endl;
+            cout << "MACAS: Mova-se em direcao as macas para come-las e crescer." << endl;
+            cout << "EVITAR COLISOES: Evite colidir com a parede ou com o corpo da cobra." << endl;
+            cout << "VITORIA: Ao comer 100 macas sem colisoes, voce vence o jogo!" << endl<< endl;
+            cout << "BOA SORTE!" << endl << endl;
+            system("pause");
+            system ("cls");
+            break;
+        case 3: //Rank
+            system ("cls");
+
+            system("pause");
+            system ("cls");
+            break;
+        case 4: // Obrigado por jogar
+
+            break;
+        default:
+            cout << "Esta opcao nao e aceita digite outro numero";
+            break;
         }
-    } while (menu != 4);
+    }
+    while (menu != 4);
 
     return 0;
 } //fim main
